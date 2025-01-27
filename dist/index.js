a7b8446077c6773d41a8f908013ae29ac80787cf
Juri Wolf <Amerlander@users.noreply.github.com>
2025-01-27 03:50:16 +0100

    refactor: clean up whitespace and improve code readability in DocxMerger

--- dist/index.js
+++ dist/index.js
@@ -23,7 +23,6 @@ function DocxMerger(options, files) {
         self._files.push(new JSZip(file));
     });
     this._contentTypes = {};
-
     this._media = {};
     this._rel = {};
 
@@ -31,21 +30,19 @@ function DocxMerger(options, files) {
 
     this.insertPageBreak = function () {
         var pb = '<w:p> \
-					<w:r> \
-						<w:br w:type="page"/> \
-					</w:r> \
-				  </w:p>';
+                    <w:r> \
+                        <w:br w:type="page"/> \
+                    </w:r> \
+                  </w:p>';
         this._builder.push(pb);
     };
 
     this.insertRaw = function (xml) {
-
         this._builder.push(xml);
     };
 
     this.insertHeadersAndFooters = function (headerFooterRefs) {
         headerFooterRefs.forEach(ref => {
-            // Insert the header/footer XML into the appropriate place
             if (ref.type === "headerReference") {
                 this._header.push(ref.xml);
             } else if (ref.type === "footerReference") {
@@ -69,29 +66,22 @@ function DocxMerger(options, files) {
         Style.mergeStyles(files, this._style);
 
         files.forEach(function (zip, index) {
-            // Extract document XML
             var xml = zip.file("word/document.xml").asText();
 
-            // Extract <w:body> content
             var bodyStartIndex = xml.indexOf("<w:body>") + 8;
             var bodyEndIndex = xml.lastIndexOf("<w:sectPr");
 
-            // Extract section properties (<w:sectPr>)
             var sectPrStartIndex = xml.lastIndexOf("<w:sectPr");
             var sectPrEndIndex = xml.indexOf("</w:sectPr>", sectPrStartIndex) + 11;
             var sectPr = xml.slice(sectPrStartIndex, sectPrEndIndex);
 
-            // Extract content inside <w:body> excluding <w:sectPr>
             var content = xml.slice(bodyStartIndex, bodyEndIndex);
 
-            // Merge headers and footers
             var headerFooterRefs = headersFooters.extractHeadersFooters(zip, sectPr);
             self.insertHeadersAndFooters(headerFooterRefs);
 
-            // Append content to the builder
             self.insertRaw(content);
 
-            // Append the section properties for this section
             if (self._pageBreak && index < files.length - 1) {
                 self.insertRaw(`<w:p><w:pPr>${sectPr}</w:pPr></w:p>`);
             }
@@ -107,13 +97,12 @@ function DocxMerger(options, files) {
 
         xml = xml.replace(xml.slice(startIndex, endIndex), this._body.join(''));
 
-        RelContentType.generateContentTypes(zip, this._contentTypes);
+        RelContentType.generateContentTypes(zip, this._contentTypes, this._header, this._footer);
         Media.copyMediaFiles(zip, this._media, this._files);
-        RelContentType.generateRelations(zip, this._rel);
+        RelContentType.generateRelations(zip, this._rel, this._header, this._footer);
         bulletsNumbering.generateNumbering(zip, this._numbering);
         Style.generateStyles(zip, this._style);
 
-        // Generate header and footer files
         headersFooters.generateHeaders(zip, this._header);
         headersFooters.generateFooters(zip, this._footer);
 
@@ -129,7 +118,6 @@ function DocxMerger(options, files) {
     };
 
     if (this._files.length > 0) {
-
         this.mergeBody(this._files);
     }
 }
--- src/merge-relations-and-content-type.js
+++ src/merge-relations-and-content-type.js
@@ -1,89 +1,148 @@
-
 var XMLSerializer = require('xmldom').XMLSerializer;
 var DOMParser = require('xmldom').DOMParser;
 
-
-var mergeContentTypes = function(files, _contentTypes) {
-
-
-    files.forEach(function(zip) {
-        // var zip = new JSZip(file);
+var mergeContentTypes = function (files, _contentTypes) {
+    files.forEach(function (zip) {
         var xmlString = zip.file("[Content_Types].xml").asText();
-        var xml = new DOMParser().parseFromString(xmlString, 'text/xml');
+        var xml = new DOMParser().parseFromString(xmlString, "text/xml");
 
-        var childNodes = xml.getElementsByTagName('Types')[0].childNodes;
+        var childNodes = xml.getElementsByTagName("Types")[0].childNodes;
 
         for (var node in childNodes) {
             if (/^\d+$/.test(node) && childNodes[node].getAttribute) {
-                var contentType = childNodes[node].getAttribute('ContentType');
-                if (!_contentTypes[contentType])
+                var contentType = childNodes[node].getAttribute("ContentType");
+                if (!_contentTypes[contentType]) {
                     _contentTypes[contentType] = childNodes[node].cloneNode();
+                }
             }
         }
-
     });
 };
 
-var mergeRelations = function(files, _rel) {
-
-    files.forEach(function(zip) {
-        // var zip = new JSZip(file);
+var mergeRelations = function (files, _rel) {
+    files.forEach(function (zip) {
         var xmlString = zip.file("word/_rels/document.xml.rels").asText();
-        var xml = new DOMParser().parseFromString(xmlString, 'text/xml');
+        var xml = new DOMParser().parseFromString(xmlString, "text/xml");
 
-        var childNodes = xml.getElementsByTagName('Relationships')[0].childNodes;
+        var childNodes = xml.getElementsByTagName("Relationships")[0].childNodes;
 
         for (var node in childNodes) {
             if (/^\d+$/.test(node) && childNodes[node].getAttribute) {
-                var Id = childNodes[node].getAttribute('Id');
-                if (!_rel[Id])
+                var Id = childNodes[node].getAttribute("Id");
+                if (!_rel[Id]) {
                     _rel[Id] = childNodes[node].cloneNode();
+                }
             }
         }
-
     });
 };
 
-var generateContentTypes = function(zip, _contentTypes) {
-    // body...
+var generateContentTypes = function (zip, _contentTypes, headers, footers) {
     var xmlString = zip.file("[Content_Types].xml").asText();
-    var xml = new DOMParser().parseFromString(xmlString, 'text/xml');
+    var xml = new DOMParser().parseFromString(xmlString, "text/xml");
     var serializer = new XMLSerializer();
 
     var types = xml.documentElement.cloneNode();
 
+    // Add merged content types
     for (var node in _contentTypes) {
         types.appendChild(_contentTypes[node]);
     }
 
+    // Add header content types
+    headers.forEach((_, index) => {
+        const override = xml.createElement("Override");
+        override.setAttribute("PartName", `/word/header${index + 1}.xml`);
+        override.setAttribute(
+            "ContentType",
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.header+xml"
+        );
+        types.appendChild(override);
+    });
+
+    // Add footer content types
+    footers.forEach((_, index) => {
+        const override = xml.createElement("Override");
+        override.setAttribute("PartName", `/word/footer${index + 1}.xml`);
+        override.setAttribute(
+            "ContentType",
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.footer+xml"
+        );
+        types.appendChild(override);
+    });
+
     var startIndex = xmlString.indexOf("<Types");
-    xmlString = xmlString.replace(xmlString.slice(startIndex), serializer.serializeToString(types));
+    xmlString = xmlString.replace(
+        xmlString.slice(startIndex),
+        serializer.serializeToString(types)
+    );
 
     zip.file("[Content_Types].xml", xmlString);
 };
 
-var generateRelations = function(zip, _rel) {
-    // body...
+var generateRelations = function (zip, _rel, headers, footers) {
     var xmlString = zip.file("word/_rels/document.xml.rels").asText();
-    var xml = new DOMParser().parseFromString(xmlString, 'text/xml');
+    var xml = new DOMParser().parseFromString(xmlString, "text/xml");
     var serializer = new XMLSerializer();
 
-    var types = xml.documentElement.cloneNode();
+    var relationships = xml.documentElement.cloneNode();
 
+    // Add merged relationships
     for (var node in _rel) {
-        types.appendChild(_rel[node]);
+        relationships.appendChild(_rel[node]);
     }
 
+    // Add header relationships
+    headers.forEach((_, index) => {
+        const relationship = xml.createElement("Relationship");
+        relationship.setAttribute("Id", `rIdHeader${index + 1}`);
+        relationship.setAttribute(
+            "Type",
+            "http://schemas.openxmlformats.org/officeDocument/2006/relationships/header"
+        );
+        relationship.setAttribute("Target", `header${index + 1}.xml`);
+        relationships.appendChild(relationship);
+    });
+
+    // Add footer relationships
+    footers.forEach((_, index) => {
+        const relationship = xml.createElement("Relationship");
+        relationship.setAttribute("Id", `rIdFooter${index + 1}`);
+        relationship.setAttribute(
+            "Type",
+            "http://schemas.openxmlformats.org/officeDocument/2006/relationships/footer"
+        );
+        relationship.setAttribute("Target", `footer${index + 1}.xml`);
+        relationships.appendChild(relationship);
+    });
+
     var startIndex = xmlString.indexOf("<Relationships");
-    xmlString = xmlString.replace(xmlString.slice(startIndex), serializer.serializeToString(types));
+    xmlString = xmlString.replace(
+        xmlString.slice(startIndex),
+        serializer.serializeToString(relationships)
+    );
 
     zip.file("word/_rels/document.xml.rels", xmlString);
 };
 
+var generateHeaders = function (zip, headers) {
+    headers.forEach((content, index) => {
+        zip.file(`word/header${index + 1}.xml`, content);
+    });
+};
 
+var generateFooters = function (zip, footers) {
+    footers.forEach((content, index) => {
+        zip.file(`word/footer${index + 1}.xml`, content);
+    });
+};
+
+// Main export
 module.exports = {
     mergeContentTypes: mergeContentTypes,
     mergeRelations: mergeRelations,
     generateContentTypes: generateContentTypes,
-    generateRelations: generateRelations
+    generateRelations: generateRelations,
+    generateHeaders: generateHeaders,
+    generateFooters: generateFooters,
 };
